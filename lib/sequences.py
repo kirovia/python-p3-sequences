8c597467ef89f578a24e78985852701d64c062a9
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2025-05-22 11:37:42 -0400

    Passes all tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        fib_list = [0, 1]
+        while len(fib_list) < length:
+            next_fib = fib_list[-1] + fib_list[-2]
+            fib_list.append(next_fib)
+        print(fib_list)
